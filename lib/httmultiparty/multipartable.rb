0e4326b354353a644ac568fcd7489534308b4980
Luis <luismartinellandreu@gmail.com>
2018-12-04 14:12:56 +0100

    Remove nested multipart

--- lib/httmultiparty/multipartable.rb
+++ lib/httmultiparty/multipartable.rb
@@ -7,8 +7,9 @@ module HTTMultiParty::Multipartable
   end
 
   def body=(value)
-    @body_parts = Array(value).map { |(k, v)| Parts::Part.new(boundary, k, v) }
-    @body_parts << Parts::EpiloguePart.new(boundary)
+    # @body_parts = Array(value).map { |(k, v)| Parts::Part.new(boundary, k, v) }
+    # @body_parts << Parts::EpiloguePart.new(boundary)
+    @body_parts = value
     set_headers_for_body
   end
 
@@ -20,9 +21,11 @@ module HTTMultiParty::Multipartable
 
   def set_headers_for_body
     if defined?(@body_parts) && @body_parts
-      set_content_type('multipart/form-data',  'boundary' => boundary)
-      self.content_length = @body_parts.inject(0) { |sum, i| sum + i.length }
-      self.body_stream = CompositeReadIO.new(*@body_parts.map(&:to_io))
+      # set_content_type('multipart/form-data',  'boundary' => boundary)
+      # self.content_length = @body_parts.inject(0) { |sum, i| sum + i.length }
+      # self.body_stream = CompositeReadIO.new(*@body_parts.map(&:to_io))
+      self.content_length = @body_parts.length
+      self.body_stream = CompositeReadIO.new(StringIO.new(@body_parts))
     end
   end
 end
